d995c80a6f8db53e527827031f4d7db5b90611c3
XavierBerger <xberger@netbook.(none)>
2013-07-29 11:15:52 +0200

    Configuration loading

--- lib/configuration.py
+++ lib/configuration.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 import sys, os, pprint
 import array
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
  
 class Configuration(object):
   def __init__(self):
@@ -12,63 +19,59 @@ class Configuration(object):
                          {'name': 'right', 'src': 'green', 'dst': 'green'},
                          {'name': 'enter', 'src': 'green', 'dst': 'green'}],
                 }
-    self.smc = {}
 
   def load(self, filename):
+    self.result = {}
+    pp = pprint.PrettyPrinter(indent=4)
     with open(filename, 'r') as f:
       for line in f:
         line = line.replace('\n','')
         if (len(line) == 0) or (line[0] == '#') : continue
-        #print line
         (key, value) = line.split('=') 
         leaves = key.split('.')
-
+        prevleaf = None
+        tree=self.result
+        for currentleaf in leaves:
+          if prevleaf == None:
+            prevleaf = currentleaf
+            continue
+          if is_number(prevleaf):
+            prevleaf=int(prevleaf)
+          if is_number(currentleaf):
+            try:
+              tree[prevleaf]
+            except:
+              try:
+                tree[prevleaf]=[]
+              except:
+                tree[prevleaf].append([])
+          else:
+            try:
+              tree[prevleaf]
+            except:
+              try:
+                tree[prevleaf]={}
+              except:
+                tree.append({})
+          #print "prevleaf=%s currentleaf=%s" % ( prevleaf, currentleaf)
+          #pp.pprint(tree)
+          tree=tree[prevleaf]
+          prevleaf = currentleaf
+        
+        if is_number(currentleaf):
+          currentleaf = int(currentleaf)
+          tree.append(value)
+        else:
+          tree[currentleaf] = value
+        
+    pp.pprint(self.result)
+          
 
 
 
 if __name__ == "__main__":
   configuration = Configuration()
   configuration.load('../rpimonitorlcd.conf')
-  pp = pprint.PrettyPrinter(indent=4)
-  pp.pprint(configuration.toto)
+  #pp = pprint.PrettyPrinter(indent=4)
+  #pp.pprint(configuration.toto)
 
-'''
-    chomp;
-    /^\s*#|^$/ and next;                 # Ignore comments
-    my ($key, $value) = /^([^=]*)=(.*)/; # Extract key and value
-    my @leaves=split('\.', $key);        # Explode key and construct config hash
-    my $tree=$this;
-    my $current;
-    my $next;
-    while (scalar(@leaves) > 0) {
-      $current ||= shift (@leaves);
-      $next = shift (@leaves);
-      if ( $next =~ /^\d+$/ ) {
-        if ($current =~ /^\d+$/) {
-          @{$tree}[$current-1] ||= [];
-          $tree=@{$tree}[$current-1];
-        }
-        else {
-          $tree->{$current} ||= [];
-          $tree=$tree->{$current};
-        }
-      } else {
-        if ($current =~ /^\d+$/) {
-          @{$tree}[$current-1] ||= {};
-          $tree=@{$tree}[$current-1];
-        }
-        else {
-          $tree->{$current} ||= {};
-          $tree=$tree->{$current};
-        }
-      }      
-      if ( ($next eq 'rrd') and $value) { push(@{$this->{'rrd'}},$tree) };
-      $current = $next;
-    }
-    if ($current =~ /^\d+$/) {
-      @{$tree}[$current-1] = $value;
-    }
-    else {
-      $tree->{$current} = $value;
-    }
-'''
