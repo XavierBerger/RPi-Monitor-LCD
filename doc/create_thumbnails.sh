77086232efa324ca17de6a166e8a5e8eb40ddffa
XavierBerger <berger.xavier@gmail.com>
2013-10-02 09:38:11 +0200

    Add I2c bus and PCB8544 pin labels

--- doc/create_thumbnails.sh
+++ doc/create_thumbnails.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for iloop in RPi-Monitor-LCD_bb.png  RPi-Monitor-LCD_pcb.png  RPi-Monitor-LCD_schem.png; do
   cp $iloop thumb_$iloop
-  mogrify -resize 50% -format png thumb_$iloop
+  mogrify -resize 600x -format png thumb_$iloop
 done
 
